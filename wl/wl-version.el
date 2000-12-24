e7cedee395406262e9f097528af80205ca1af8a3
kaoru <kaoru>
2000-12-24 09:54:40 +0000

    * wl-version.el (wl-version-status-alist): Use `=' instead of `eq'.

--- wl/wl-version.el
+++ wl/wl-version.el
@@ -61,7 +61,7 @@ If ARG insert string at point."
     (message "%s" (wl-version t))))
 
 (defvar wl-version-status-alist
-  '(((eq (% (nth 1 (product-version (product-find 'wl-version))) 2) 0)
+  '(((= (% (nth 1 (product-version (product-find 'wl-version))) 2) 0)
      . "stable")
     (t . "beta"))
   "An alist to define the version status.")
