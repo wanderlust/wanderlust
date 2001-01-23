a6d812d1ff3a6e622d1c20d87eeb5eae4eae2e4f
kaoru <kaoru>
2001-01-23 11:48:39 +0000

    * wl-version.el (wl-version-show): Removed. (wl-version): Add `interactive'.

--- elmo/elmo-version.el
+++ elmo/elmo-version.el
@@ -39,7 +39,7 @@
 
 ;; product-define in the first place
 (product-provide 'elmo-version
-  (product-define "ELMO" nil '(2 5 6)))
+  (product-define "ELMO" nil '(2 5 5)))
 
 ;; For APEL 10.2 or earlier.
 (defun-maybe product-version-as-string (product)
