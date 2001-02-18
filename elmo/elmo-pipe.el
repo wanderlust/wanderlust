30f281d32bad0147fef6e5cee6059bc643674ce9
okazaki <okazaki>
2001-02-18 21:04:01 +0000

    Update.

--- elmo/elmo-pipe.el
+++ elmo/elmo-pipe.el
@@ -70,7 +70,7 @@
       (elmo-commit src))
     (run-hooks 'elmo-pipe-drained-hook)))
 
-(defun elmo-pipe-list-folder (spec &optional nohide)
+(defun elmo-pipe-list-folder (spec)
   (elmo-pipe-drain (elmo-pipe-spec-src spec)
 		   (elmo-pipe-spec-dst spec))
   (let ((killed (and elmo-use-killed-list
