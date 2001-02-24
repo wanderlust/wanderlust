592b66104750936998f43b66343f038b1d3aa07b
kaoru <kaoru>
2001-02-24 15:43:26 +0000

    Fix example hostname. (RFC2606) Reflect Martin's patch. [WL 6924]

--- wl/wl-summary.el
+++ wl/wl-summary.el
@@ -2924,7 +2924,7 @@ If ARG, without confirm."
 
 (defun wl-summary-default-subject-filter (subject)
   (let ((case-fold-search t))
-    (setq subject (elmo-replace-in-string subject "[ \t]*\\(re\\|was\\)[:>]" ""))
+    (setq subject (elmo-replace-in-string subject "[ \t]*\\(re\\|was\\):" ""))
     (setq subject (elmo-replace-in-string subject "[ \t]" ""))
     (elmo-replace-in-string subject "^\\[.*\\]" "")))
 
