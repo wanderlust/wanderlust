168a2858ca30e9716a370eb11c6d4362a691fedc
kaoru <kaoru>
2001-04-22 03:09:23 +0000

    * wl-version.el (wl-version-status): New variable. (wl-version-status-alist): Removed. (wl-version-status): Rule included.

--- wl/wl-vars.el
+++ wl/wl-vars.el
@@ -32,6 +32,7 @@
 ;;
 
 (require 'elmo-vars)
+(require 'elmo-util)
 
 (if (module-installed-p 'custom)
     (require 'custom))
@@ -1551,7 +1552,8 @@ Each elements are regexp of folder name."
   '(("^-alt\\.chinese" . big5)
     ("^-relcom\\." . koi8-r)
     ("^-tw\\." . big5)
-    ("^-han\\." . euc-kr))
+    ("^-han\\." . euc-kr)
+    ("@sponichi" . shift_jis))
   "Charset alist.  If no match, `wl-mime-charset' is used."
   :type '(repeat (cons (regexp :tag "Folder Regexp") (symbol :tag "Charset")))
   :group 'wl-summary
