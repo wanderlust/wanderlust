782a4e5e3055495a8fc0adefd20d54232533cf13
teranisi <teranisi>
2001-06-15 09:18:45 +0000

    * wl-version.el (wl-version): Changed codename.
    * elmo-version.el (elmo-version): Up to 2.6.0.

--- wl/wl-folder.el
+++ wl/wl-folder.el
@@ -1290,14 +1290,14 @@ If current line is group folder, all subfolders are marked."
     (if gbw
 	(progn (select-window gbw)
 	       (setq exists t))
-      (unless wl-summary-use-frame
+      (unless wl-folder-use-frame
 	(condition-case ()
 	    (unwind-protect
 		(split-window-horizontally wl-folder-window-width)
 	      (other-window 1))
 	  (error nil))))
     (set-buffer buffer)
-    (if wl-summary-use-frame
+    (if wl-folder-use-frame
 	(switch-to-buffer-other-frame buffer)
       (switch-to-buffer buffer))
     exists))
@@ -1434,24 +1434,11 @@ Entering Folder mode calls the value of `wl-folder-mode-hook'."
 
 (defun wl-folder (&optional arg)
   (interactive "P")
-  (let (initialize folder-buf)
-    (if (setq folder-buf (get-buffer wl-folder-buffer-name))
-	(if wl-folder-use-frame
-	    (let (select-frame)
-	      (save-selected-window
-		(dolist (frame (visible-frame-list))
-		  (select-frame frame)
-		  (if (get-buffer-window folder-buf)
-		      (setq select-frame frame))))
-	      (if select-frame
-		  (select-frame select-frame)
-		(switch-to-buffer folder-buf)))
-	  (switch-to-buffer folder-buf))
-      (if wl-folder-use-frame
-	  (switch-to-buffer-other-frame
-	   (get-buffer-create wl-folder-buffer-name))
-	(switch-to-buffer (get-buffer-create wl-folder-buffer-name)))
-      (switch-to-buffer (get-buffer wl-folder-buffer-name))
+  (let (initialize)
+;;; (delete-other-windows)
+    (if (get-buffer wl-folder-buffer-name)
+	(switch-to-buffer  wl-folder-buffer-name)
+      (switch-to-buffer (get-buffer-create wl-folder-buffer-name))
       (wl-folder-mode)
       (wl-folder-init)
       (set-buffer wl-folder-buffer-name)
@@ -2083,8 +2070,6 @@ Use `wl-subscribed-mailing-list'."
 	    (if (eq 'imap4 (elmo-folder-get-type folder))
 		(elmo-imap4-spec-mailbox (elmo-imap4-get-spec folder))
 	      (substring folder 1)))
-      (if (string-match "@" folder)
-	  (setq folder (substring folder 0 (match-beginning 0))))
       (when (string-match "[^\\./]+$" folder) ; last hierarchy
 	(setq key (regexp-quote
 		   (concat (substring folder (match-beginning 0)) "@")))
