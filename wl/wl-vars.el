b36edac84c7941c9ff3287827fa756f66e557a7c
teranisi <teranisi>
2001-06-21 00:36:21 +0000

    * elmo.el (elmo-generic-folder-append-messages): Fixed logic. (Detected by NAKAJIMA Mikio <minakaji@osaka.email.ne.jp>)

--- wl/wl-vars.el
+++ wl/wl-vars.el
@@ -42,12 +42,7 @@
 (defgroup wl nil
   "Wanderlust, a news and mail reading software."
   :tag "Wanderlust"
-  :link (` (custom-manual
-	    (, (if (and (boundp 'current-language-environment)
-			(string-equal "Japanese"
-				      (symbol-value 'current-language-environment)))
-		   "(wl-ja)Top"
-		 "(wl)Top"))))
+  :link '(custom-manual "(wl-ja)Top")
   :group 'news
   :group 'mail)
 
@@ -733,12 +728,9 @@ Default is for 'followup-to-me'."
   :type '(repeat string)
   :group 'wl-summary)
 
-(defcustom wl-summary-fix-timezone nil
-  "*Time zone of the date string in summary mode. 
-If nil, it is adjust to the default time zone information
-\(system's default time zone or environment variable TZ\)."
-  :type '(choice (const :tag "Default time zone" nil)
-		 string)
+(defcustom wl-summary-fix-timezone "JST"
+  "Non-nil forces to fix timezone of summary date."
+  :type 'string
   :group 'wl-summary)
 
 (defcustom wl-summary-default-score 0
@@ -1454,12 +1446,12 @@ every intervals specified by wl-biff-check-interval."
   :type 'integer
   :group 'wl-highlight)
 
-(defcustom wl-biff-state-indicator-on "[¢©]"
+(defcustom wl-biff-state-indicator-on "[$B")(B]"
   "String used to show biff status ON."
   :type 'string
   :group 'wl-highlight)
 
-(defcustom wl-biff-state-indicator-off "[¡¾]"
+(defcustom wl-biff-state-indicator-off "[$B!>(B]"
   "String used to show biff status OFF."
   :type 'string
   :group 'wl-highlight)
@@ -2161,15 +2153,15 @@ a symbol `bitmap', `xbm' or `xpm' in order to force the image format."
 ;; Advanced thread view.
 (defvar wl-thread-indent-level 1
   "*Indent level for thread.")
-(defvar wl-thread-have-younger-brother-str "¨²"
+(defvar wl-thread-have-younger-brother-str "$B(2(B"
   "*A string for thread branch line.  It should contain one character.")
-(defvar wl-thread-youngest-child-str       "¨±"
+(defvar wl-thread-youngest-child-str       "$B(1(B"
   "*A string for thread branch line.  It should contain one character.")
-(defvar wl-thread-vertical-str             "¨­"
+(defvar wl-thread-vertical-str             "$B(-(B"
   "*A string for thread branch line.  It should contain one character.")
-(defvar wl-thread-horizontal-str           "¨¬"
+(defvar wl-thread-horizontal-str           "$B(,(B"
   "*A string for thread branch line.  It should contain one character.")
-(defvar wl-thread-space-str                "¡¡"
+(defvar wl-thread-space-str                "$B!!(B"
   "*A string for thread branch line.  It should contain one character.")
 
 (defvar wl-highlight-thread-indent-string-regexp "[^[<]*"
