c6f5c34a60d93697e06ce4c266af1c0b128b62ad
hmurata <hmurata>
2001-08-16 12:32:06 +0000

    * elmo-mime.el (elmo-mime-display-as-is-internal): Put text property 'mime-preview-situation for `mime-preview-quit'.

--- wl/wl-summary.el
+++ wl/wl-summary.el
@@ -2075,9 +2075,10 @@ If ARG is non-nil, checking is omitted."
 	  (when delete-list
 	    (wl-summary-delete-messages-on-buffer delete-list "Deleting...")
 	    (message "Deleting...done"))
-	  (wl-summary-set-status-marks-on-buffer
-	   wl-summary-new-mark
-	   wl-summary-unread-uncached-mark)
+	  (when new-msgdb
+	    (wl-summary-set-status-marks-on-buffer
+	     wl-summary-new-mark
+	     wl-summary-unread-uncached-mark))
 	  (setq append-list (elmo-msgdb-get-overview new-msgdb))
 	  (setq curp append-list)
 	  (setq num (length curp))
