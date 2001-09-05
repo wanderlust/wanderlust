e388c3bd43721dfb7670d4783fcc58c24cad62f8
yamaoka <yamaoka>
2001-09-05 12:40:03 +0000

    * wl-e21.el (wl-highlight-folder-current-line): Use Posix style character  classes and Perl style shy-groups for regexp. (wl-highlight-plugged-current-line): Use Posix style character classes for  regexp. (wl-e21-highlight-folder-by-numbers): Ditto. (wl-e21-display-image-p): Use `display-images-p' instead of  `display-graphic-p'. (display-images-p): Make an alias to `display-graphic-p' if it is not   available.  It is for Emacs version prior to 21.0.105.
    * wl-demo.el (wl-demo): Use `display-images-p' instead of `display-graphic-p'.
    (wl-demo-image-type-alist): Ditto.
    (display-images-p): Make an alias to `display-graphic-p' if it is not
     available.  It is for Emacs version prior to 21.0.105.

--- wl/wl-acap.el
+++ wl/wl-acap.el
@@ -29,11 +29,13 @@
 ;;
 
 (cond
+ ((and (not (featurep 'utf-2000))
+       (module-installed-p 'un-define))
+  (require 'un-define))
  ((and (featurep 'xemacs)
+       (not (featurep 'utf-2000))
        (module-installed-p 'xemacs-ucs))
-  (require 'xemacs-ucs))
- ((module-installed-p 'un-define)
-  (require 'un-define)))
+  (require 'xemacs-ucs)))
 (require 'custom)
 (require 'cus-edit)
 (require 'wl-vars)
@@ -136,14 +138,22 @@ If nil, default acap port is used."
 				       (memq 'string type)))
 			      (if (memq sym wl-acap-base64-encode-options)
 				  (wl-acap-base64-decode-string (cadr x))
-				(cadr x)))
+				(decode-coding-string
+				 (cadr x)
+				 wl-acap-coding-system)))
 			     (t
 			      (if (cadr x)
 				  (read
 				   (if (memq sym
 					     wl-acap-base64-encode-options)
 				       (wl-acap-base64-decode-string (cadr x))
-				     (read (concat "\"" (cadr x) "\""))))))))))
+				      (read (concat 
+					     "\""
+					     (decode-coding-string
+					      (cadr x)
+					      wl-acap-coding-system)
+					     "\""))
+				      ))))))))
 			(t 'wl-acap-ignored))))
 		   settings)))
     ;; Setup options.
@@ -259,11 +269,15 @@ If nil, default acap port is used."
 		      (if (memq option wl-acap-base64-encode-options)
 			  (wl-acap-base64-encode-string
 			   (symbol-value option))
-			(symbol-value option)))
+			(encode-coding-string
+			 (symbol-value option)
+			 wl-acap-coding-system)))
 		     (t (if (memq option wl-acap-base64-encode-options)
 			    (wl-acap-base64-encode-string
 			     (prin1-to-string (symbol-value option)))
-			  (prin1-to-string (symbol-value option))))))
+			  (encode-coding-string
+			   (prin1-to-string (symbol-value option))
+			   wl-acap-coding-system)))))
 		  settings)))
     (unwind-protect
 	(progn
