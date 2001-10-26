b1c1373f7463a2f25130b2b1c6aedac3c7deb7a6
kaoru <kaoru>
2001-10-26 16:41:01 +0000

    * check-modules.el (check-modules-x-face-xmas): New testcase. For old x-face utility. (check-modules-x-face-xmas-wl-display-x-face-was-argument-required): New function.

--- tests/check-modules.el
+++ tests/check-modules.el
@@ -12,3 +12,18 @@
   (require 'apel-ver)
   (lunit-assert
    (product-version>= (product-find 'apel-ver) '(10 2))))
+
+;; X-Face on XEmacs
+(luna-define-method check-modules-x-face-xmas ((case check-modules))
+  (when (and (locate-library "x-face") (featurep 'xemacs))
+    (lunit-assert
+     (check-modules-x-face-xmas-wl-display-x-face-was-argument-required))))
+
+(defun check-modules-x-face-xmas-wl-display-x-face-was-argument-required ()
+  "When `x-face-xmas-wl-display-x-face' has non-optional argument, return `t'."
+  (require 'x-face-xmas)
+  (condition-case nil
+      (with-temp-buffer
+	(x-face-xmas-wl-display-x-face)
+	t)
+    (wrong-number-of-arguments)))
