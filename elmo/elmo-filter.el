4aa0f49c13f2252a93c90bee96eb356a87cb0bac
hmurata <hmurata>
2002-10-01 07:47:26 +0000

    * elmo-filter.el (elmo-folder-list-unreads): Call generic method if require-msgdb slot is nil. (elmo-folder-list-importants): Likewise.

--- elmo/elmo-filter.el
+++ elmo/elmo-filter.el
@@ -176,8 +176,11 @@
    (elmo-folder-list-unreads
     (elmo-filter-folder-target-internal folder))))
 
-(luna-define-method elmo-folder-list-unreads ((folder elmo-filter-folder))
-  (elmo-filter-folder-list-unreads folder))
+(luna-define-method elmo-folder-list-unreads :around ((folder
+						       elmo-filter-folder))
+  (if (elmo-filter-folder-require-msgdb-internal folder)
+      (elmo-filter-folder-list-unreads folder)
+    (luna-call-next-method)))
 
 (defsubst elmo-filter-folder-list-importants (folder)
   (elmo-list-filter
@@ -185,8 +188,11 @@
    (elmo-folder-list-importants
     (elmo-filter-folder-target-internal folder))))
 
-(luna-define-method elmo-folder-list-importants ((folder elmo-filter-folder))
-  (elmo-filter-folder-list-importants folder))
+(luna-define-method elmo-folder-list-importants :around ((folder
+							  elmo-filter-folder))
+  (if (elmo-filter-folder-require-msgdb-internal folder)
+      (elmo-filter-folder-list-importants folder)
+    (luna-call-next-method)))
 
 (luna-define-method elmo-folder-list-subfolders ((folder elmo-filter-folder)
 						 &optional one-level)
