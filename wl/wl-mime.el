1a4ce80456d9411cbf5bb0bc387e8074ec1dc59d
teranisi <teranisi>
2002-10-22 05:20:58 +0000

    * wl-util.el (wl-region-exists-p): New function.
    * wl-mime.el (wl-draft-yank-current-message-entity): Use it;
    Remove defvars for byte-compile warnings.
    
    * elmo.el (elmo-generic-folder-diff): Avoid byte-compile warning.

--- wl/wl-mime.el
+++ wl/wl-mime.el
@@ -34,14 +34,6 @@
 (require 'mime-play)
 (require 'elmo)
 
-(eval-when-compile
-  (defalias-maybe 'Meadow-version 'ignore))
-
-(defvar xemacs-betaname)
-(defvar xemacs-codename)
-(defvar enable-multibyte-characters)
-(defvar mule-version)
-
 ;;; Draft
 
 (defalias 'wl-draft-editor-mode 'mime-edit-mode)
@@ -68,12 +60,9 @@ has Non-nil value\)"
 	  (set-buffer (wl-current-message-buffer))
 	  (save-restriction
 	    (widen)
-	      (if (or (and wl-on-xemacs
-			   zmacs-regions zmacs-region-active-p)
-		      (and (not wl-on-xemacs)
-			   transient-mark-mode mark-active))
-		  (wl-mime-preview-follow-current-region)
-		(mime-preview-follow-current-entity)))))))
+	    (if (wl-region-exists-p)
+		(wl-mime-preview-follow-current-region)
+	      (mime-preview-follow-current-entity)))))))
 
 ;; modified mime-preview-follow-current-entity from mime-view.el
 (defun wl-mime-preview-follow-current-region ()
