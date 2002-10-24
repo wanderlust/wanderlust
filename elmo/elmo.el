61f157673e18d9e0eada22c0662f31ee544e64dc
hmurata <hmurata>
2002-10-24 17:36:09 +0000

    * elmo-msgdb.el (elmo-msgdb-set-flag): Use `elmo-file-cache-exists-p'; use proper mark that depends on `use-cache'. (elmo-msgdb-unset-flag): Ditto. (elmo-msgdb-set-cached): Ditto. added argument `use-cache'.
    * elmo.el (elmo-message-set-cached): Call `elmo-msgdb-set-cached'
    with 4th argument `use-cache'.

--- elmo/elmo.el
+++ elmo/elmo.el
@@ -1114,8 +1114,10 @@ Return a cons cell of (NUMBER-CROSSPOSTS . NEW-MARK-ALIST).")
 FOLDER is the ELMO folder structure.
 NUMBER is a number of the message.
 If CACHED is t, message mark is set as cached."
-  (when (elmo-msgdb-set-cached
-	 (elmo-folder-msgdb folder) number cached)
+  (when (elmo-msgdb-set-cached (elmo-folder-msgdb folder)
+			       number
+			       cached
+			       (elmo-message-use-cache-p folder number))
     (elmo-folder-set-mark-modified-internal folder t)))
 
 
