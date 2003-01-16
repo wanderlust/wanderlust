e18d8309ed45a5e58c90c6eea0efbb6b173ff8a0
yoichi <yoichi>
2003-01-16 09:25:30 +0000

    Merge from trunk: * elmo-localdir.el (elmo-folder-expand-msgdb-path): Correct msgdb path for "+~/something".

--- elmo/elmo-localdir.el
+++ elmo/elmo-localdir.el
@@ -91,8 +91,9 @@
     'identity
     (mapcar
      'elmo-replace-string-as-filename
-     (split-string (elmo-localdir-folder-dir-name-internal folder)
-		   "/"))
+     (split-string
+      (expand-file-name (elmo-localdir-folder-dir-name-internal folder))
+      "/"))
     "/")
    (expand-file-name ;;"localdir"
     (symbol-name (elmo-folder-type-internal folder))
