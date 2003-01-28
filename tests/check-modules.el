334c659878ba1c0eb483466299c431d3f8f7a773
kaoru <kaoru>
2003-01-28 22:14:50 +0000

    * check-modules.el (test-apel-version): Renamed from `check-modules-apel-version'. (test-modules-charsets-mime-charset-alist): Ditto. (test-x-face-xmas, test-x-face-mule): Ditto. (test-flim-mime-entity, test-flim-mime-content-information): Ditto. (test-flim-encoded-word, test-flim-content-transfer-encoding): Ditto. (test-flim-mailcap, test-flim-std11, test-flim-smtp): Ditto. (test-semi-mime-edit, test-semi-mime-view, test-semi-mime-play): Ditto.
    * check-base64.el (test-base64-encode-1):
    Renamed from `check-base64-encode-1'.
    (test-base64-encode-2, test-base64-encode-3): Ditto.
    (test-base64-encode-4, test-base64-encode-5): Ditto.

--- tests/check-modules.el
+++ tests/check-modules.el
@@ -8,18 +8,18 @@
 ;;;
 
 ;; APEL
-(luna-define-method check-modules-apel-version ((case check-modules))
+(luna-define-method test-apel-version ((case check-modules))
   (require 'apel-ver)
   (lunit-assert
    (product-version>= (product-find 'apel-ver) '(10 3))))
 
-(luna-define-method check-modules-charsets-mime-charset-alist ((case check-modules))
+(luna-define-method test-modules-charsets-mime-charset-alist ((case check-modules))
   (require 'mcharset)
   (lunit-assert
    (not (memq nil charsets-mime-charset-alist))))
 
 ;; X-Face on XEmacs
-(luna-define-method check-modules-x-face-xmas ((case check-modules))
+(luna-define-method test-x-face-xmas ((case check-modules))
   (when (and (locate-library "x-face") (featurep 'xemacs))
     (lunit-assert
      (check-modules-x-face-xmas-wl-display-x-face-was-argument-required))))
@@ -34,7 +34,7 @@
     (wrong-number-of-arguments)))
 
 ;; x-face-mule
-(luna-define-method check-modules-x-face-mule ((case check-modules))
+(luna-define-method test-x-face-mule ((case check-modules))
   (when (and (locate-library "x-face-mule")
 	     (not (featurep 'xemacs))
 	     (not (locate-library "x-face-e21")))
@@ -52,7 +52,7 @@
     (wrong-number-of-arguments)))
 
 ;; MIME entity (FLIM API Version 1.14 Draft Release 3)
-(luna-define-method check-modules-flim-mime-entity ((case check-modules))
+(luna-define-method test-flim-mime-entity ((case check-modules))
   (require 'mime)
   (lunit-assert (fboundp 'mime-open-entity)) ; [Required]<Suggest>
   (lunit-assert (fboundp 'mime-entity-children)) ; [Required]<Suggest>
@@ -67,7 +67,7 @@
   (lunit-assert (fboundp 'mime-write-entity-body))) ; [Required]
 
 ;; MIME content information (FLIM API Version 1.14 Draft Release 3)
-(luna-define-method check-modules-flim-mime-content-information ((case check-modules))
+(luna-define-method test-flim-mime-content-information ((case check-modules))
   (require 'mime)
   (lunit-assert (fboundp 'mime-content-type-primary-type)) ; [Required]
   (lunit-assert (fboundp 'mime-content-type-subtype)) ; [Required]
@@ -76,13 +76,13 @@
   (lunit-assert (fboundp 'mime-content-disposition-parameter))) ; [Required]
 
 ;; encoded-word (FLIM API Version 1.14 Draft Release 3)
-(luna-define-method check-modules-flim-encoded-word ((case check-modules))
+(luna-define-method test-flim-encoded-word ((case check-modules))
   (require 'mime)
   (lunit-assert (fboundp 'mime-decode-field-body)) ; [Required]<Suggest>
   (lunit-assert (fboundp 'mime-encode-field-body))) ; [Required]<Suggest>
 
 ;; Content-Transfer-Encoding (FLIM API Version 1.14 Draft Release 3)
-(luna-define-method check-modules-flim-content-transfer-encoding ((case check-modules))
+(luna-define-method test-flim-content-transfer-encoding ((case check-modules))
   (require 'mel)
   ;; mime-decode-string (string encoding) [Required]<Suggest>
   (lunit-assert (fboundp 'mime-decode-string))
@@ -98,7 +98,7 @@
   (lunit-assert (fboundp 'binary-insert-encoded-file))) ; [Required]
 
 ;; Mailcap (FLIM API Version 1.14 Draft Release 3)
-(luna-define-method check-modules-flim-mailcap ((case check-modules))
+(luna-define-method test-flim-mailcap ((case check-modules))
   (require 'mime-conf)
   (lunit-assert (fboundp 'mime-parse-mailcap-buffer)) ; [Required]<Suggest>
   (lunit-assert (boundp 'mime-mailcap-file)) ; [Required]<Suggest>
@@ -106,7 +106,7 @@
   (lunit-assert (fboundp 'mime-format-mailcap-command))) ; [Required]<Suggest>
 
 ;; STD 11 (FLIM API Version 1.14 Draft Release 3)
-(luna-define-method check-modules-flim-std11 ((case check-modules))
+(luna-define-method test-flim-std11 ((case check-modules))
   (require 'std11)
   (lunit-assert (fboundp 'std11-narrow-to-header)) ; [Required]
   (lunit-assert (fboundp 'std11-fetch-field)) ; [Required]
@@ -114,13 +114,13 @@
   (lunit-assert (fboundp 'std11-unfold-string))) ; [Required]
 
 ;; SMTP (FLIM API Version 1.14 Draft Release 3)
-(luna-define-method check-modules-flim-smtp ((case check-modules))
+(luna-define-method test-flim-smtp ((case check-modules))
   (require 'smtp)
   (lunit-assert (fboundp 'smtp-send-buffer))) ; [Suggest]
 
 
 ;; SEMI
-(luna-define-method check-modules-semi-mime-edit ((case check-modules))
+(luna-define-method test-semi-mime-edit ((case check-modules))
   (require 'mime-edit)
   (lunit-assert (fboundp 'mime-find-file-type))
   (lunit-assert (fboundp 'mime-edit-content-beginning))
@@ -128,7 +128,7 @@
   (lunit-assert (fboundp 'mime-edit-preview-message))
   (lunit-assert (fboundp 'mime-create-tag)))
 
-(luna-define-method check-modules-semi-mime-view ((case check-modules))
+(luna-define-method test-semi-mime-view ((case check-modules))
   (require 'mime-view)
   (lunit-assert (fboundp 'mime-display-message))
   (lunit-assert (fboundp 'mime-maybe-hide-echo-buffer))
@@ -138,6 +138,6 @@
   (lunit-assert (fboundp 'mime-display-text/plain))
   (lunit-assert (fboundp 'mime-entity-situation)))
 
-(luna-define-method check-modules-semi-mime-play ((case check-modules))
+(luna-define-method test-semi-mime-play ((case check-modules))
   (require 'mime-play)
   (lunit-assert (fboundp 'mime-store-message/partial-piece)))
