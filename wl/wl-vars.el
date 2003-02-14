632c633176c70cc847aabee5c3fe8693e06cb082
yoichi <yoichi>
2003-02-14 12:43:06 +0000

    * wl-vars.el (wl-folder-hierarchy-access-folders): Change default value.

--- wl/wl-vars.el
+++ wl/wl-vars.el
@@ -2176,9 +2176,10 @@ POLICY is copy or move."
   :group 'wl-summary
   :group 'wl-pref)
 
-(defcustom wl-folder-hierarchy-access-folders '("^-$" "^-alt$")
+(defcustom wl-folder-hierarchy-access-folders '("^-[^\\.]*\\(:\\|@\\|$\\)"
+						"^@$")
   "*Access group REGEXPs to make hierarchy structure."
-  :type '(repeat (string :tag "Folder"))
+  :type '(repeat (string :tag "Regexp"))
   :group 'wl-folder)
 
 (defcustom wl-folder-init-load-access-folders nil
