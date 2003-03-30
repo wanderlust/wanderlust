9e982584eef725ee56e0542ee301d4ba4d73fd75
yoichi <yoichi>
2003-03-30 09:28:18 +0000

    * wl/wl-fldmgr.el (wl-fldmgr-delete): Move confirmation to elmo side, cut visible folder when elmo-folder-delete returns non-nil value. * elmo/elmo.el (elmo-folder-delete): Confirm deletion here, return t if the folder has been deleted successfully. * elmo/elmo-imap4.el (elmo-folder-delete): Ditto. * elmo/elmo-archive.el (elmo-folder-delete): Ditto. * elmo/elmo-localdir.el (elmo-folder-delete): Ditto. * elmo/elmo-maildir.el (elmo-folder-delete): Ditto.

--- wl/wl-fldmgr.el
+++ wl/wl-fldmgr.el
@@ -850,15 +850,8 @@ return value is diffs '(-new -unread -all)."
     (let* ((inhibit-read-only t)
 	   (tmp (wl-fldmgr-get-path-from-buffer))
 	   (entity (elmo-string (nth 4 tmp)))
-	   (folder (wl-folder-get-elmo-folder entity))
-	   (msgs (and (elmo-folder-exists-p folder)
-		      (elmo-folder-list-messages folder))))
-      (when (yes-or-no-p (format "%sDo you really want to delete \"%s\"? "
-				 (if (> (length msgs) 0)
-				     (format "%d msg(s) exists. " (length msgs))
-				   "")
-				 entity))
-	(elmo-folder-delete folder)
+	   (folder (wl-folder-get-elmo-folder entity)))
+      (when (elmo-folder-delete folder)
 	(wl-fldmgr-cut tmp nil t)))))
 
 (defun wl-fldmgr-rename ()
