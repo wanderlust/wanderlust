549f764f2d363a9ce76282d7d699abb0fea2a1d0
teranisi <teranisi>
2003-08-02 14:58:51 +0000

    Version number is increased to 2.11.5.

--- elmo/elmo-version.el
+++ elmo/elmo-version.el
@@ -40,7 +40,7 @@
 ;; product-define in the first place
 (product-provide 'elmo-version
   ;; Don't forget to check `wl-version.el' and Info.
-  (product-define "ELMO" nil '(2 11 4)))
+  (product-define "ELMO" nil '(2 11 5)))
 
 ;; set version-string
 (product-version-as-string 'elmo-version)
