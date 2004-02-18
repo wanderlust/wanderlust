9a9594c1ec757362170b8e4fbf1295f58e1ae1c3
kaoru <kaoru>
2004-02-18 14:37:41 +0000

    * test-elmo-localdir.el (test-elmo-folder-expand-msgdb-path-4): New testcase. (test-elmo-folder-expand-msgdb-path-2): Change order.

--- tests/test-elmo-localdir.el
+++ tests/test-elmo-localdir.el
@@ -15,8 +15,8 @@
 (luna-define-method test-elmo-folder-expand-msgdb-path-2 ((case test-elmo-localdir))
   (lunit-assert
    (string=
-    (elmo-folder-expand-msgdb-path (wl-folder-get-elmo-folder "+inbox"))
-    (expand-file-name "localdir/inbox" elmo-msgdb-directory))))
+    (expand-file-name "localdir/inbox" elmo-msgdb-directory)
+    (elmo-folder-expand-msgdb-path (wl-folder-get-elmo-folder "+inbox")))))
 
 
 (luna-define-method test-elmo-folder-expand-msgdb-path-3 ((case test-elmo-localdir))
@@ -25,3 +25,11 @@
     (string=
      (elmo-folder-expand-msgdb-path (wl-folder-get-elmo-folder "+/inbox"))
      (elmo-folder-expand-msgdb-path (wl-folder-get-elmo-folder "+inbox"))))))
+
+(luna-define-method test-elmo-folder-expand-msgdb-path-4 ((case test-elmo-localdir))
+  (lunit-assert
+   (string=
+    (expand-file-name "localdir/inbox" elmo-msgdb-directory)
+    (elmo-folder-expand-msgdb-path
+     (wl-folder-get-elmo-folder
+      (concat "+" elmo-localdir-folder-path "/inbox"))))))
