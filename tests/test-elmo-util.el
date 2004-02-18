11e127164a8db0563335cebe7f9614497eb8928c
kaoru <kaoru>
2004-02-18 15:32:03 +0000

    * test-elmo-util.el (test-elmo-object-save-1): New testcase. (lunit-test-case-setup, lunit-test-case-teardown): Add temporary-file stuff.

--- tests/test-elmo-util.el
+++ tests/test-elmo-util.el
@@ -1,7 +1,19 @@
 (require 'lunit)
 (require 'elmo-util)
 
+
 (luna-define-class test-elmo-util (lunit-test-case))
+
+;; setup & teardown
+(defvar test-elmo-temoporary-file)
+
+(luna-define-method lunit-test-case-setup ((case test-elmo-util))
+  (setq test-elmo-temoporary-file
+	(make-temp-file temporary-file-directory)))
+
+(luna-define-method lunit-test-case-teardown ((case test-elmo-util))
+  (delete-file test-elmo-temoporary-file))
+
 
 (luna-define-method test-elmo-replace-string-as-filename-1 ((case test-elmo-util))
   (lunit-assert
@@ -9,3 +21,13 @@
      (string= str
 	      (elmo-recover-string-from-filename
 	       (elmo-replace-string-as-filename str))))))
+
+
+(luna-define-method test-elmo-object-save-1 ((case test-elmo-util))
+  (let ((list '(1 2 3 4 5 6 7 8 9 10 11 12))
+	(print-length 1)
+	(print-level 1))
+    (elmo-object-save test-elmo-temoporary-file list)
+    (lunit-assert
+     (equal list
+	    (elmo-object-load test-elmo-temoporary-file)))))
