1d33210329d0731473efb196b6279b4b6344b6ae
hmurata <hmurata>
2004-05-22 10:30:21 +0000

    Version number is increased to 2.11.29.

--- elmo/elmo-version.el
+++ elmo/elmo-version.el
@@ -40,7 +40,7 @@
 ;; product-define in the first place
 (product-provide 'elmo-version
   ;; Don't forget to check `wl-version.el' and Info.
-  (product-define "ELMO" nil '(2 11 28)))
+  (product-define "ELMO" nil '(2 11 29)))
 
 ;; set version-string
 (product-version-as-string 'elmo-version)
