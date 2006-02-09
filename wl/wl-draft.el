c49e625a8d6daee554635e4acce96d934665a56c
yoichi <yoichi>
2006-02-09 03:57:39 +0000

    * wl-draft.el (wl-smtp-extension-bind): Don't bind smtp-end-of-line.

--- wl/wl-draft.el
+++ wl/wl-draft.el
@@ -147,10 +147,6 @@ e.g.
 	     (if (eq wl-smtp-connection-type 'ssl)
 		 #'open-ssl-stream
 	       smtp-open-connection-function))
-	    (smtp-end-of-line
-	     (if (eq wl-smtp-connection-type 'ssl)
-		 "\n"
-	       smtp-end-of-line))
 	    smtp-sasl-user-name smtp-sasl-properties sasl-read-passphrase)
        (setq smtp-sasl-user-name wl-smtp-posting-user
 	     smtp-sasl-properties (when wl-smtp-authenticate-realm
