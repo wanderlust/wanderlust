588a8c951db39243c1646adbb92299c4d7c88293
kaoru <kaoru>
2008-01-01 00:32:35 +0000

    * wl-demo.el (wl-demo-copyright-notice): Add 2008.

--- wl/wl-demo.el
+++ wl/wl-demo.el
@@ -31,7 +31,7 @@
 ;;; Code:
 
 (defconst wl-demo-copyright-notice
-  "Copyright (C) 1998-2007 Yuuichi Teranishi <teranisi@gohome.org>"
+  "Copyright (C) 1998-2008 Yuuichi Teranishi <teranisi@gohome.org>"
   "A declaration of the copyright on Wanderlust.")
 
 (eval-when-compile
