7d41106fe271b4a1d5f277d5e56a09b6effb6dd2
kaoru <kaoru>
2008-02-19 10:48:01 +0000

    * test-wl-util.el (test-wl-unique-id-by-user): New testcase. (test-wl-inverse-alist): Ditto.

--- tests/test-wl-util.el
+++ tests/test-wl-util.el
@@ -16,3 +16,17 @@
    (not
     (string= (wl-unique-id)
 	     (progn (sleep-for 1) (wl-unique-id))))))
+
+(luna-define-method test-wl-unique-id-by-user ((case test-wl-util))
+  (lunit-assert
+   (let (user-login-name)
+     (not
+      (string= (progn (setq user-login-name "_alice") (wl-unique-id))
+	       (progn (setq user-login-name "_bob") (wl-unique-id)))))))
+
+
+(luna-define-method test-wl-inverse-alist ((case test-wl-util))
+  (lunit-assert
+   (equal
+    '((cc c) (aa a))
+    (wl-inverse-alist '(a c) '((a . aa) (a . bb) (c . cc))))))
