f5ddd2a4453dc66b9e3773c87d3b01b25c73255b
kaoru <kaoru>
2009-08-01 02:34:48 +0000

    * test-utf7.el (toplevel): Use `fboundp' instead of `boundp'. Fix (require 'utf7) order for Mule-UCS. Add coding: comment.

--- tests/test-utf7.el
+++ tests/test-utf7.el
@@ -1,11 +1,14 @@
+;; -*- coding: iso-2022-jp -*-
 (require 'lunit)
+
+;; Emacs 21
+(unless (and (fboundp 'find-coding-system) (find-coding-system 'utf-16))
+  (ignore-errors (require 'un-define)))
 (require 'utf7)
 
-;; Emacs 21.3.50 or later
-(if (boundp 'utf-translate-cjk-mode)
-    (utf-translate-cjk-mode 1)
-  ;; Use Mule-UCS if installed
-  (ignore-errors (require 'un-define)))
+;; Emacs 21.3.50 to 22
+(when (fboundp 'utf-translate-cjk-mode)
+  (utf-translate-cjk-mode 1))
 
 (luna-define-class test-utf7 (lunit-test-case))
 
