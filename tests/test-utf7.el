4a49dfcd344822c3be3613037794bb7900156094
kaoru <kaoru>
2009-08-01 05:00:44 +0000

    (test-utf7-decode-string-alpha): New testcase. (test-utf7-encode-string-alpha): Ditto. (test-utf7-decode-string-smiling-face): Ditto. (test-utf7-encode-string-smiling-face): Ditto.

--- tests/test-utf7.el
+++ tests/test-utf7.el
@@ -20,6 +20,25 @@
 	     (make-char 'japanese-jisx0208 75 92)
 	     (make-char 'japanese-jisx0208 56 108))))))
 
+(luna-define-method test-utf7-encode-string-smiling-face ((case test-utf7))
+  (lunit-assert
+   (string=
+    "Hi Mom -+Jjo--!"
+    (utf7-encode-string
+     (concat "Hi Mom -"
+	     (string (make-char 'mule-unicode-2500-33ff 35 58))
+	     "-!")))))
+
+(luna-define-method test-utf7-encode-string-alpha ((case test-utf7))
+  (lunit-assert
+   (string=
+    "A+ImIDkQ-."
+    (utf7-encode-string
+     (concat "A"
+	    (string (make-char 'mule-unicode-0100-24ff 121 34)
+		    (make-char 'mule-unicode-0100-24ff 38 113))
+	     ".")))))
+
 (luna-define-method test-utf7-encode-string-plus ((case test-utf7))
   (lunit-assert
    (string= "+-" (utf7-encode-string "+"))))
@@ -42,6 +61,31 @@
 	    (make-char 'japanese-jisx0208 75 92)
 	    (make-char 'japanese-jisx0208 56 108))
     (utf7-decode-string "+ZeVnLIqe-"))))
+
+(luna-define-method test-utf7-decode-string-smiling-face ((case test-utf7))
+  (lunit-assert
+   (string=
+    (concat "Hi Mom -"
+	    (string (make-char 'mule-unicode-2500-33ff 35 58))
+	    "-!")
+    (utf7-decode-string "Hi Mom -+Jjo--!"))))
+
+(luna-define-method test-utf7-decode-string-alpha ((case test-utf7))
+  (lunit-assert
+   (string=
+    (concat "A"
+	    (string (make-char 'mule-unicode-0100-24ff 121 34)
+		    (make-char 'mule-unicode-0100-24ff 38 113))
+	    ".")
+    (utf7-decode-string "A+ImIDkQ.")))	; omit `-'
+  ;;
+  (lunit-assert
+   (string=
+    (concat "A"
+	    (string (make-char 'mule-unicode-0100-24ff 121 34)
+		    (make-char 'mule-unicode-0100-24ff 38 113))
+	    ".")
+    (utf7-decode-string "A+ImIDkQ-."))))
 
 (luna-define-method test-utf7-decode-string-plus ((case test-utf7))
   (lunit-assert
