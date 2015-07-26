b6df160a34518a5c4dd3e4ba4b45e2668bb212a4
David Maus <dmaus@ictsoc.de>
2015-07-26 11:56:44 +0200

    Rewrite elmo-imap4 tests to use Emacs' Ert test library

--- tests/test-elmo-imap4.el
+++ tests/test-elmo-imap4.el
@@ -1,33 +1,50 @@
-(require 'lunit)
+(require 'ert)
 (require 'elmo-imap4)
 
-(luna-define-class test-elmo-imap4 (lunit-test-case))
+(ert-deftest test-elmo-imap4-parse-greeting/ok ()
+  (should (eq 'nonauth (with-temp-buffer
+                         (let (case-fold-search)
+                           (insert-string "* OK [CAPABILITY IMAP4 IMAP4rev1 LITERAL+ ID"
+                                          " LOGINDISABLED AUTH=DIGEST-MD5 AUTH=CRAM-MD5 SASL-IR]"
+                                          " mail.example.org Cyrus IMAP v2.3.13 server ready\n")
+                           (goto-char (point-min))
+                           (elmo-imap4-parse-greeting))))))
 
-(luna-define-method test-elmo-imap4-parse-greeting-ok ((case test-elmo-imap4))
-  (with-temp-buffer
-    (setq case-fold-search nil)
-    (let (elmo-imap4-status)
-      (insert-string "* OK [CAPABILITY IMAP4 IMAP4rev1 LITERAL+ ID"
-		     " LOGINDISABLED AUTH=DIGEST-MD5 AUTH=CRAM-MD5 SASL-IR]"
-		     " mail.example.org Cyrus IMAP v2.3.13 server ready\n")
-      (goto-char (point-min))
-      (lunit-assert
-       (eq 'nonauth (elmo-imap4-parse-greeting))))))
+(ert-deftest test-elmo-imap4-parse-greeting/preauth ()
+  (should (eq 'auth (with-temp-buffer
+                      (let (case-fold-search)
+                        (insert-string "* PREAUTH IMAP4rev1 server logged in as Smith\n")
+                        (goto-char (point-min))
+                        (elmo-imap4-parse-greeting))))))
 
-(luna-define-method test-elmo-imap4-parse-greeting-preauth ((case test-elmo-imap4))
-  (with-temp-buffer
-    (setq case-fold-search nil)
-    (let (elmo-imap4-status)
-      (insert-string "* PREAUTH IMAP4rev1 server logged in as Smith\n")
-      (goto-char (point-min))
-      (lunit-assert
-       (eq 'auth (elmo-imap4-parse-greeting))))))
+(ert-deftest test-elmo-imap4-parse-greeting/bye ()
+  (should (eq 'closed (with-temp-buffer
+                        (let (case-fold-search)
+                          (insert-string "* BYE LOGOUT received\n")
+                          (goto-char (point-min))
+                          (elmo-imap4-parse-greeting))))))
 
-(luna-define-method test-elmo-imap4-parse-greeting-bye ((case test-elmo-imap4))
-  (with-temp-buffer
-    (setq case-fold-search nil)
-    (let (elmo-imap4-status)
-      (insert-string "* BYE LOGOUT received\n")
-      (goto-char (point-min))
-      (lunit-assert
-       (eq 'closed (elmo-imap4-parse-greeting))))))
+(ert-deftest test-elmo-imap4-flatten-command/unknown-token-type ()
+  (should
+   (string-match-p
+    "^Invalid token type:"
+    (error-message-string
+     (should-error (elmo-imap4-flatten-command '((invalid))))))))
+
+(ert-deftest test-elmo-imap4-flatten-command/unknown-command-token ()
+  (should
+   (string-match-p
+    "^Invalid command token:"
+    (error-message-string
+     (should-error (elmo-imap4-flatten-command '(3)))))))
+
+(ert-deftest test-elmo-imap4-flatten-command/ok ()
+  (let ((cmd (elmo-imap4-flatten-command (list "a" (list 'atom "b") (list 'quoted "c") (list 'literal "d") "e f"))))
+    (should (string= (nth 0 cmd) "a b \"c\""))
+    (should (listp (nth 1 cmd)))
+    (should (string= (nth 2 cmd) "e f"))))
+
+(ert-deftest test-elmo-imap4-flatten-command/parens ()
+  (should (string= "a (b c) d" (nth 0 (elmo-imap4-flatten-command (list "a" "(" "b" "c" ")" "d")))))
+  (should (string= "a (" (nth 0 (elmo-imap4-flatten-command (list "a" "(" (list 'literal "b") ")" "c")))))
+  (should (string= ") c" (nth 2 (elmo-imap4-flatten-command (list "a" "(" (list 'literal "b") ")" "c"))))))
