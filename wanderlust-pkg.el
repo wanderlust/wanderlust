4334120c24748431988e268a9718b0823b089ec6
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2020-10-27 21:41:15 +0900

    Update package description file for MELPA.
    * wanderlust-pkg.el: Define supported Emacs version.  Explicitly
    define dependency on apel and flim, because Wanderlust actually
    have direct dependency on FLIM-LB and APEL-LB.

--- wanderlust-pkg.el
+++ wanderlust-pkg.el
@@ -1,3 +1,6 @@
 (define-package "wanderlust" "2.15.9"
   "Yet Another Message Interface on Emacsen"
-  '((semi "1.14.7")))
+  '((emacs "24.5")
+    (apel "10.8")
+    (flim "1.14.9")
+    (semi "1.14.7")))
