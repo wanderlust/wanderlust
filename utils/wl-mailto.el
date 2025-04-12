693c6e02781411897fb708c43cc69e90b9c94cf7
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2025-04-12 20:57:51 +0900

    Add supports for rfc6068.el on wl-mailto.el
    wl-mailto.el now uses rfc6068.el if available.  RFC 6068 is
    successor to RFC 2368.  The library is bundled with Eemacs 28 and
    later.
    
    * utils/wl-mailto.el: Use rfc6068.el when available.  Drop XEmacs
    support.
    (wl-mailto-version): Bump up to 0.6
    (wl-mailto-parse-function): New variable.
    (wl-mailto-compose-message-from-mailto-url): Use it.  Don't validate
    argument because parser function does.
    (wl-mailto-compose-message-from-mailto-url-at-point): Don't validate
    URL.

--- utils/wl-mailto.el
+++ utils/wl-mailto.el
@@ -3,7 +3,7 @@
 ;;; Copyright (C) 1999 Sen Nagata
 
 ;; Author: Sen Nagata <sen@eccosys.com>
-;; Version: 0.5
+;; Version: 0.6
 ;; License: GPL 2
 ;; Warning: not optimized at all
 
@@ -15,7 +15,7 @@
 ;;
 ;;   -wl (>= 0.9.6?)
 ;;
-;;   -rfc2368.el
+;;   -rfc6068.el or rfc2368.el
 ;;   -thingatpt.el or browse-url.el
 ;;
 ;; installation:
@@ -50,6 +50,10 @@
 
 ;;; History:
 ;;
+;; 0.6
+;;
+;;   Support rfc6068.el
+;;
 ;; 0.5
 ;;
 ;;   wl-user-agent functionality merged into wl-draft.el, so removed
@@ -76,55 +80,47 @@
 ;;   rewrote to use rfc2368.el and wl-user-agent.el
 
 ;;; Code:
-(defconst wl-mailto-version "wl-mailto.el 0.5")
+(defconst wl-mailto-version "wl-mailto.el 0.6")
 
-;; how should we handle the dependecy on wl?
-;; will this work?
-(eval-when-compile
-  (require 'wl)
-  (defun wl-mailto-url-at-point ()))
+(require 'elmo-util)
+(require 'wl-draft)
 
+;; use rfc6068 or rfc2368 support -- should be usable for things other
+;; than wl too
+(unless (require 'rfc6068 nil t)
+  (require 'rfc2368))
 
-;; use rfc2368 support -- should be usable for things other than wl too
-(require 'rfc2368)
+(declare-function rfc6068-parse-mailto-url "rfc6068" (mailto-url))
+(declare-function rfc2368-parse-mailto-url "rfc2368" (mailto-url))
 
-;; yucky compatibility stuff -- someone help me w/ this, please...
-(if (and (string-match "^XEmacs \\([0-9.]+\\)" (emacs-version))
-	 (< (string-to-number (match-string 1 (emacs-version))) 21))
-    ;; for xemacs versions less than 21, use browse-url.el
-    (progn
-      (require 'browse-url)
-      (fset 'wl-mailto-url-at-point
-	    'browse-url-url-at-point))
-  ;; for everything else, use thingatpt.el
-  (progn
-    (require 'thingatpt)
-    (fset 'wl-mailto-url-at-point
-	  (lambda ()
-	    (thing-at-point 'url)))))
+(defvar wl-mailto-parse-function (if (featurep 'rfc6068)
+				     #'rfc6068-parse-mailto-url
+				   #'rfc2368-parse-mailto-url))
 
 (defun wl-mailto-compose-message-from-mailto-url (url &optional _dummy)
   "Compose a message from URL (RFC 2368).
 The optional second argument, DUMMY, exists to match the interface
 provided by `browse-url-mail' (w3) -- DUMMY does not do anything."
   (interactive "sURL: ")
-  (if (string-match rfc2368-mailto-regexp url)
-      (let* ((other-headers (rfc2368-parse-mailto-url url))
-	     (to (cdr (elmo-assoc-ignore-case "to" other-headers)))
-	     (subject (cdr (elmo-assoc-ignore-case "subject" other-headers))))
-
-	(wl-user-agent-compose to subject other-headers))
-    (message "Not a mailto: url.")))
+  (let ((result (ignore-errors
+		  ;; rfc2368-parse-mailto-url signals error when url
+		  ;; is not a mailto: URL.
+		  (funcall wl-mailto-parse-function url))))
+    (if result
+	(let ((to (cdr (elmo-assoc-ignore-case "to" result)))
+	      (subject (cdr (elmo-assoc-ignore-case "subject" result))))
+	  (wl-user-agent-compose to subject result))
+      (message "Not a mailto: url"))))
 
 ;; prepare a message from a mailto: url at point
 (defun wl-mailto-compose-message-from-mailto-url-at-point ()
   "Draft a new message based on URL (RFC 2368) at point."
   (interactive)
-  (let ((url (wl-mailto-url-at-point)))
-    (if (and url (string-match rfc2368-mailto-regexp url))
+  (let ((url (thing-at-point 'url)))
+    (if url
 	(wl-mailto-compose-message-from-mailto-url url)
       ;; tell the user that we didn't find a mailto: url at point
-      (message "No mailto: url detected at point."))))
+      (message "No url detected at point"))))
 
 ;; since this will be used via 'require'...
 (provide 'wl-mailto)
