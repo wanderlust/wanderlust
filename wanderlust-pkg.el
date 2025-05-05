2f6f7c7b19ba9942bff94dccaa9415a12ee3549b
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2025-05-05 16:17:43 +0900

    Update package archive related stuff
    * wanderlust-pkg.el: Add :maintainers property.
    
    * wanderlust-utils-pkg.el: New file.

--- wanderlust-pkg.el
+++ wanderlust-pkg.el
@@ -3,4 +3,5 @@
   '((emacs "24.5")
     (apel "10.8")
     (flim "1.14.9")
-    (semi "1.14.7")))
+    (semi "1.14.7"))
+  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
---
+++ wanderlust-utils-pkg.el
@@ -0,0 +1,5 @@
+(define-package "wanderlust-utils" "2.15.9"
+  "Utilities for Wanderlust"
+  '((emacs "24.5")
+    (wanderlust "2.15.9"))
+  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
